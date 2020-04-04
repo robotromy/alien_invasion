e39e9be47d2b64dd22a46c3d873f597a5a1af2d3
robotromy <erick.rubiol@protonmail.com>
2020-04-04 10:37:37 -0600

    Se crea el repositorio alien_invasion

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+
+    def __init__(self):
+        # Initialize the game settings
+        self.screen_width = 1366
+        self.screen_height = 768
+        self.bg_color = (230, 230, 230)
